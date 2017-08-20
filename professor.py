5dcb1101d2cc266ee43b8eee8b6fc2af07543dd9
SuperLavrik <superlavrik@gmail.com>
2017-08-20 19:32:36 +0300

    Update professor.py

--- professor.py
+++ professor.py
@@ -4,11 +4,46 @@ class Professor(Person):
 
     def __init__(self, name, age):
         super().__init__(name, age)
-    #     self.name = name
-    #     self.age = age
-    #
+        self._salary = 0
+        self._groups = []
+        self.awards = []
+
+
+    def add_groups(self, groups):
+        if len(self.gruops) > 3 :
+            raise ValueError("Too much assigments!")
+        self._groups = groups
+
+
+    def remove_groups(self, gruop):
+        if group in groups:
+            self._groups.remove(gruop)
+
+    def def_group(self):
+
+
+
     # def print_info(self):
+    #     super().print_info()
+    #     print('Salary:', self.salary)
+    #     if ien(self.groups) > 0:
+    #         print('Groups :', self.groups)
+    #     print( "Scientific awards: ", self.awards)
     #     print('================================')
-    #     print('Name:', self.name)
-    #     print('Age:', self.age)
-    #     print('================================')
+
+     def print_info_ext(self):
+        print('Salary:', self._salary)
+        if len(self._groups) > 0:
+            print('Groups :', self._groups)
+        print("Scientific awards: ", self.awards)
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, salary):
+        if salary < 0:
+            raise ValueError("Salary can't be negative !")
+        self._salary = salary
+
